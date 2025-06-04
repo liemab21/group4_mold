55eb62702f79ac3b38b959abbfd3cd5fbf234e91
Benjamin Senekowisch <122978402+senbeb21@users.noreply.github.com>
2025-06-04 14:37:12 +0200

    Initialized Project

---
+++ mold/src/main/java/com/co2/mold/MoldApplication.java
@@ -0,0 +1,13 @@
+package com.co2.mold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoldApplication.class, args);
+	}
+
+}
---
+++ mold/src/test/java/com/co2/mold/MoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co2.mold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
